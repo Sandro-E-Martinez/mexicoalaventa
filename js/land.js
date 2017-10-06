752b2e07eca5b2a8f73495e823954ec5c136fc96
SandroMartinez <zanddro@gmail.com>
2017-10-06 18:46:48 -0500

    ahora consume el API de google y se comprimieron archivos para una mejor carga

--- js/land.js
+++ js/land.js
@@ -3,10 +3,29 @@ selector: 'img'
 });			
 
 
+function initMap() {
+    var pos = {lat: 19.243352, lng: -99.165322};
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+     	zoom: 15,
+     	center: pos          
+    });
+
+	var propertieCircle = new google.maps.Circle({
+            strokeColor: '#F75111',
+            strokeWeight: 1,
+            fillColor: '#F75111',
+            fillOpacity: 0.35,
+            map: map,
+            center: pos,
+            radius: 500
+      });
+ }
+
+
 $(function() {
 
-
-	$loader = $('<div class="loader">');
+	$loader = $('<div class="loader">');	
 
 	function endCleanForm(){
 		$loader.remove();
@@ -16,6 +35,25 @@ $(function() {
 		$('#buyerTel').val('');
 		$('#buyerComment').val('');
 	}
+
+	$('#listFeatures button').click(function(){
+		$('#listFeatures')
+			.find('button.button-active')
+			.removeClass('button-active')
+		$(this).addClass('button-active');
+
+		$("#features")
+			.find('div.show')
+			.removeClass('show')
+			.addClass('hide');
+
+		$newFeature = 	$('#feature-article')
+			.find('div.' +  $(this).data("feature"));
+
+		$newFeature.toggleClass('hide');
+		$newFeature.addClass('show');
+	});
+
 
 	$('#buyerForm')
 		.submit(function(ev){
